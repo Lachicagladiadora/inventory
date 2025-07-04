b2ce2c33f2e9fcfea810b6a4e6da4277a3eb4fb4
Lachicagladiadora <dev.roseny@gmail.com>
2025-07-04 17:17:33 -0500

    doc: documentation and test endpoints

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -4,8 +4,10 @@ import { validateProduct } from "../../utils/validateProduct.utils";
 
 export const prerender = false;
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request, locals }) => {
   try {
+    const user = locals.user;
+    if (!user) throw Error("You need access for this content");
     const body = await request.json();
     if (!body) throw Error("Exist an error in the product information");
     //validar que sea lo que quiero
--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,8 +1,10 @@
 import type { APIRoute } from "astro";
 import { db, Product } from "astro:db";
 
-export const GET: APIRoute = async (): Promise<Response> => {
+export const GET: APIRoute = async (context): Promise<Response> => {
   try {
+    const user = context.locals.user;
+    if (!user) throw Error("You need access for this content");
     const products = await db.select().from(Product);
     console.log({ products });
     return new Response(JSON.stringify(products), { status: 200 });
